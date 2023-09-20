b3f1f7eaaecd8b8559afc674640caa096745c709
Quedale <sebaslau@gmail.com>
2023-09-20 11:51:22 -0400

    Fixed cmake build following refactoring CMake now supports out-of-tree build CMake now support linux client-test.c - Fixed following refactoring

--- example/client-test.c
+++ example/client-test.c
@@ -1,6 +1,7 @@
 #include "onvif_device.h"
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 
 int main(int argc, char** argv)
@@ -12,31 +13,39 @@ int main(int argc, char** argv)
         OnvifDevice_authenticate(dev);
         
         // printf("dev : %s\n",dev->ip);
+		OnvifDeviceService * device_service = OnvifDevice__get_device_service(dev);
+		OnvifMediaService * media_service = OnvifDevice__get_media_service(dev);
 
-        OnvifDeviceInformation * devinfo = OnvifDevice__device_getDeviceInformation(dev);
+        OnvifDeviceInformation * devinfo = OnvifDeviceService__getDeviceInformation(device_service);
         if (devinfo){
-			OnvifProfiles * profiles = OnvifDevice_get_profiles(dev);
-			for (int i = 0; i < profiles->sizeSrofiles; i++){
-				printf("Profile name: %s\n", profiles->profiles[i].name);
-				printf("Profile token: %s\n", profiles->profiles[i].token);
+			OnvifProfiles * profiles = OnvifMediaService__get_profiles(media_service);
+			for (int i = 0; i < OnvifProfiles__get_size(profiles); i++){
+				OnvifProfile * profile = OnvifProfiles__get_profile(profiles,i);
+				printf("Profile name: %s\n", OnvifProfile__get_name(profile));
+				printf("Profile token: %s\n", OnvifProfile__get_token(profile));
 
-				printf("StreamUri : %s\n",OnvifDevice__media_getStreamUri(dev, i));
-				printf("SnapshotUri : %s\n",OnvifDevice__media_getSnapshotUri(dev, i));
+				char * stream_uri = OnvifMediaService__getStreamUri(media_service, i);
+				char * snapshot_uri = OnvifMediaService__getSnapshotUri(media_service, i);
+				printf("StreamUri : %s\n", stream_uri);
+				printf("SnapshotUri : %s\n",snapshot_uri);
 
-				struct chunk * image_buf = OnvifDevice__media_getSnapshot(dev, i);
-				if (image_buf->size){
+				OnvifSnapshot * snapshot = OnvifMediaService__getSnapshot(media_service, i);
+				if (snapshot && OnvifSnapshot__get_size(snapshot) > 0){
 					char name[24];
 					snprintf(name, sizeof(name), "%d.jpg",i);
 					FILE* image = fopen(name, "wb");
-					fwrite(image_buf->buffer, image_buf->size, 1, image);
+					fwrite(OnvifSnapshot__get_buffer(snapshot), OnvifSnapshot__get_size(snapshot), 1, image);
 					fclose(image);
-					free(image_buf->buffer);
-					free(image_buf);
 				}
+
+				free(stream_uri);
+				free(snapshot_uri);
+				OnvifSnapshot__destroy(snapshot);
 			}
 			OnvifProfiles__destroy(profiles);
         }       
 
+		OnvifDeviceInformation__destroy(devinfo);
         OnvifDevice__destroy(dev);
         return 0;
 
